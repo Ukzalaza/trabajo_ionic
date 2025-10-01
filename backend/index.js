e16ae950b03dc2d93a2069f28c48c97d4cbb4fe4
Ukzalaza <netsuteruv2@gmail.com>
2025-10-01 16:22:48 +0100

    Frontend y backend

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+// Prueba
+app.use(express.json());
+
+app.use(express.urlencoded({ extende: true}));
+
+app.get("/", (req, res) => {
+
+    res.json({ message: "Welcome to TV application."});
+
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+
+    console.log(`Server is running in port ${PORT}.`);
+
+} );
